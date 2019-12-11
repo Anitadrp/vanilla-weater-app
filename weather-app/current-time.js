8eb0c0885f8cb9e12b0e1cd5615352798f461cb0
Anitadrp <57149081+Anitadrp@users.noreply.github.com>
2019-12-11 10:15:51 +0000

    new time js
    adding seperate js file for current time

---
+++ weather-app/current-time.js
@@ -0,0 +1,35 @@
+alert("hello");
+
+//let currentCityScreen = document.querySelector(".currentCityScreen");
+let currentTime = document.createElement("p");
+currentCityScreen.appendChild(currentTime);
+
+let date = new Date();
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[date.getDay()];
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+let month = months[date.getMonth()];
+
+currentTime.innerHTML = `${day}, ${date.getDate()} ${month} ${date.getFullYear()}<br>${date.toLocaleTimeString()}`;
--- weather-app/index.js
+++ weather-app/index.js
@@ -1,40 +1,8 @@
 let currentCityScreen = document.querySelector(".currentCityScreen");
 let currentCity = document.createElement("h1");
 currentCityScreen.appendChild(currentCity);
-let currentTime = document.createElement("p");
-currentCityScreen.appendChild(currentTime);
 
 currentCity.innerHTML = "hello";
-
-let date = new Date();
-const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
-let day = days[date.getDay()];
-
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-];
-let month = months[date.getMonth()];
-
-currentTime.innerHTML = `${day}, ${date.getDate()} ${month} ${date.getFullYear()}<br>${date.toLocaleTimeString()}`;
 
 function displayCity(event) {
   event.preventDefault();
