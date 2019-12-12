743a70ecdec9484b122216a0467d53159c8dbb0a
Anitadrp <57149081+Anitadrp@users.noreply.github.com>
2019-12-12 09:31:16 +0000

    adding js functions

--- weather-app/current-time.js
+++ weather-app/current-time.js
@@ -1,35 +1,36 @@
-alert("hello");
+//alert("hello");
+function displayTime() {
+  let currentCityScreen = document.querySelector(".currentCityScreen");
+  let currentTime = document.createElement("p");
+  currentCityScreen.appendChild(currentTime);
 
-//let currentCityScreen = document.querySelector(".currentCityScreen");
-let currentTime = document.createElement("p");
-currentCityScreen.appendChild(currentTime);
+  let date = new Date();
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[date.getDay()];
 
-let date = new Date();
-const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
-let day = days[date.getDay()];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  let month = months[date.getMonth()];
 
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-];
-let month = months[date.getMonth()];
-
-currentTime.innerHTML = `${day}, ${date.getDate()} ${month} ${date.getFullYear()}<br>${date.toLocaleTimeString()}`;
+  currentTime.innerHTML = `${day}, ${date.getDate()} ${month} ${date.getFullYear()}<br>${date.toLocaleTimeString()}`;
+}
--- weather-app/index.js
+++ weather-app/index.js
@@ -2,19 +2,91 @@ let currentCityScreen = document.querySelector(".currentCityScreen");
 let currentCity = document.createElement("h1");
 currentCityScreen.appendChild(currentCity);
 
-currentCity.innerHTML = "hello";
+currentCity.innerHTML = "Faro";
+
+displayTime();
 
 function displayCity(event) {
   event.preventDefault();
-  const citySearch = document.querySelector(".citySearch");
-  const currentCity = citySearch.value;
+  const citySearch = document.querySelector(".citySearch"); //search-bar
+  const city = { name: citySearch.value };
   citySearch.value = "";
 
-  alert(currentCity);
+  setCity(city);
+}
+
+function setCity(city) {
+  const apiKey = "3fceae23dde22994db28dbf0244f6a96";
+  let apiUrl;
+  if ("id" in city) {
+    apiUrl = `https://api.openweathermap.org/data/2.5/weather?id=${city.id}&units=metric&appid=${apiKey}`;
+  } else {
+    apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.name}&units=metric&appid=${apiKey}`;
+  }
+
+  axios.get(apiUrl).then(handleCityTemperature);
+}
+
+function handleCityTemperature(response) {
+  let cityElement = currentCity;
+  cityElement.innerHTML = response.data.name;
+  //alert(currentCity);
+  let cityTemperature = response.data.main.temp;
+  currentTemperature = Math.round(cityTemperature);
+  temperatureChange();
+  descriptionCurrentWeather(response);
+}
+
+let currentTemperature = 0;
+let isCelcius = true;
+
+function temperatureChange() {
+  let temperature = document.querySelector(".temperature");
+  if (isCelcius) {
+    temperature.innerHTML = `${currentTemperature}˚C`;
+  } else {
+    let fahrenheitTemperature = Math.round(currentTemperature * (9 / 5) + 32);
+    temperature.innerHTML = `${fahrenheitTemperature}˚F`;
+  }
+}
+
+function descriptionCurrentWeather(response) {
+  let description = document.querySelector(".description");
+  const wind = response.data.wind.speed;
+  const humidity = response.data.main.humidity;
+  description.innerHTML = `Wind speed: ${wind} m/s<br>Humidity: ${humidity} %`;
 }
 
 let search = document.querySelector("button");
 search.addEventListener("click", displayCity);
+
+function currentPosition() {
+  navigator.geolocation.getCurrentPosition(currentPositionCoords);
+}
+
+function currentPositionCoords(position) {
+  let apiKey = "3fceae23dde22994db28dbf0244f6a96";
+  console.log(position);
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(handleCurrentPosition);
+}
+
+function handleCurrentPosition(response) {
+  const currentCity = {
+    id: response.data.id,
+    name: "Current location"
+  };
+
+  //setFavourites([currentCity].concat(favouriteCities));
+
+  handleCityTemperature(response);
+}
+
+let currentLocation = document.querySelector(".currentLocation");
+currentLocation.addEventListener("click", currentPosition);
 
 let body = document.querySelector("body");
 let gitLink = document.createElement("a");
